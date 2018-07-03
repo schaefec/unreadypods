b7d9157a19165b31604c447d3d28f02c74e358a1
Christian Schaefer <christian.schaefer@hpe.com>
2018-07-03 09:47:21 +0200

    Add grep and namespace support

--- readypods.sh
+++ readypods.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
+if [ ! "$KUBECTL_PLUGINS_GLOBAL_FLAG_NAMESPACE" = "" ]
+    then
+       NS="-n $KUBECTL_PLUGINS_GLOBAL_FLAG_NAMESPACE"
+    else
+       NS="--all-namespaces"
+fi
 
-kubectl get pods
+
+"$KUBECTL_PLUGINS_CALLER" get pods $NS | grep -v '\s\+\([0-9]\+\)/\1\s\+'
+
+ 
